7a565b31b72cde55491668dfb4dcfdfbda668e07
cklxx <1293822641@qq.com>
2026-01-12 11:27:26 +0800

    Add math rendering support

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,12 @@
 import { defineConfig } from "astro/config";
+import rehypeKatex from "rehype-katex";
+import remarkMath from "remark-math";
 
 export default defineConfig({
   site: "https://cklxx.github.io/cklxx",
   base: "/cklxx",
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
 });
